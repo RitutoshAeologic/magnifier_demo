2c1a37ad39916475164cef0b75ddc39d4e7cefb6
Ritutosh Bhatia <102280685+RitutoshAeologic@users.noreply.github.com>
2023-03-09 17:17:34 +0530

    exploring RawMagnifier()

---
+++ lib/ui/magnifier_demo.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MagnifierDemo extends StatefulWidget {
+  const MagnifierDemo({Key? key}) : super(key: key);
+
+  @override
+  State<MagnifierDemo> createState() => _MagnifierDemoState();
+}
+
+class _MagnifierDemoState extends State<MagnifierDemo> {
+  String? data;
+
+  void loadData() async {
+    final loadedData = await rootBundle.loadString('assets/text/sample.txt');
+    setState(() {
+      data = loadedData;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  Offset dragGesturePosition = Offset.zero;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Raw Magnifier"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child:
+          Stack(
+            children: <Widget>[
+              GestureDetector(
+                  onPanUpdate: (DragUpdateDetails details) =>
+                      setState(
+                            () {
+                          dragGesturePosition = details.localPosition;
+                        },
+                      ),
+                  child: Container(
+                    // adding margin
+                    margin: const EdgeInsets.all(10.0),
+                    // adding padding
+                    padding: const EdgeInsets.symmetric(vertical: 10.0,horizontal: 15),
+                    // height should be fixed for vertical scrolling
+                    height: MediaQuery.of(context).size.height*0.9,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      // adding borders around the widget
+                      border: Border.all(
+                        color: Colors.blueAccent,
+                        width: 2.0,
+                      ),
+                    ),
+                    child:  Text( data ?? 'Text File Loading',
+                    ),
+                  )
+
+              ),
+              Positioned(
+                left: dragGesturePosition.dx,
+                top: dragGesturePosition.dy,
+                child: const RawMagnifier(
+                  decoration: MagnifierDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black, width: 3),
+                    ),
+                  ),
+                  size: Size(180, 70),
+                  magnificationScale: 1.3,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
